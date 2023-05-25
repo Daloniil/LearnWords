f198e461d95af876ae1e2b16708730d5c4fa6ae4
Daloniil <62095873+Daloniil@users.noreply.github.com>
2023-05-25 14:48:42 +0300

    Update index.tsx

--- pages/enter/index.tsx
+++ pages/enter/index.tsx
@@ -62,8 +62,7 @@ const EnterPage = () => {
         englishWord: yup.string().required("This Field Cannot Be Empty"),
         russianWord: yup.string().required("This Field Cannot Be Empty"),
     });
-    alert("Как дела")
-    const {
+    const {пр
         register,
         handleSubmit,
         formState: {errors},
